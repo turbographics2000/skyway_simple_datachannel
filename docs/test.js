b636201024a51bc06eacd43ac87e8c16c7e03639
turbographics2000 <turbographics2000@yahoo.co.jp>
2017-06-13 17:51:22 +0900

    commit

---
+++ docs/test.js
@@ -0,0 +1,45 @@
+var peer = new Peer({ key: 'ce16d9aa-4119-4097-a8a5-3a5016c6a81c', /*debug: 3*/ });
+var dc = null;
+
+peer.on('open', id => {
+    console.log('peer "onopen"');
+    myIdDisp.textContent = id;
+    getRmoteIds();
+});
+
+peer.on('connection', function (dc) {
+    dc.on('open', function () {
+        inputMessage.style.display = '';
+        inputMessage.onkeyup = evt => {
+            if (evt.keyCode === 13) {
+                if (inputMessage.value) {
+                    dc.send(inputMessage.value);
+                }
+                inputMessage.value = '';
+            }
+        }
+        console.log('dc "onopen"');
+        dc.on('data', function (data) {
+            console.log('Received', data);
+        });
+        // メッセージを送信
+        conn.send('Hello!');
+    });
+});
+
+btnStart.onclick = evt {
+    dc = peer.connect('dest-peer-id');
+};
+
+function getRmoteIds() {
+    var getRemoteIdSIId = null;
+    getRemoteIdSIId = setInterval(_ => {
+        fetch(`https://skyway.io/active/list/${apiKey}`).then(res => res.json()).then(list => {
+            var remoteIds = list.filter(memberId => memberId !== myId);
+            if (remoteIds.length) {
+                callTo.value = remoteIds[0];
+                clearInterval(getRemoteIdSIId);
+            }
+        });
+    }, 1000);
+}

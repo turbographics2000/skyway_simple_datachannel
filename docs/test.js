f5eadbce093fc69d03cea93a5c80f6b541b17252
turbographics2000 <turbographics2000@yahoo.co.jp>
2017-06-13 18:03:22 +0900

    commit

--- docs/test.js
+++ docs/test.js
@@ -1,10 +1,11 @@
+var myId = null;
 var apiKey = 'ce16d9aa-4119-4097-a8a5-3a5016c6a81c';
 var peer = new Peer({ key: apiKey, /*debug: 3*/ });
 var dc = null;
 
 peer.on('open', id => {
     console.log('peer "onopen"');
-    myIdDisp.textContent = id;
+    myIdDisp.textContent = myId = id;
     btnStart.style.display = '';
     getRmoteIds();
 });
